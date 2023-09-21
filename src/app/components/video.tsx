e097e446b6919d1e257c095f27376f435239c561
stark5200 <a.taha5200@gmail.com>
2023-09-21 22:59:19 +0200

    added video code

--- src/app/components/video.tsx
+++ src/app/components/video.tsx
@@ -15,3 +15,25 @@ const CustomImage = ({source}: PrivateProp) => {
 }
 
 export default CustomImage
+
+import { useEffect, useRef } from 'react';
+
+function VideoPlayer = () {
+  const cloudinaryRef = useRef();
+  const videoRef = useRef();
+
+  useEffect(() => {
+    if ( cloudinaryRef.current ) return;
+
+    cloudinaryRef.current = window.cloudinary;
+    cloudinaryRef.current.videoPlayer(videoRef.current, {
+      cloud_name: 'colbycloud-examples'
+    })
+  }, []);
+
+  return (
+      <video
+        ref={videoRef}
+      />
+  );
+}
