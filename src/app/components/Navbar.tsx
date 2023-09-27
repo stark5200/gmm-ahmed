d6ce998829f22f51cbbbc19bcb4b5fe52b32929e
stark5200 <a.taha5200@gmail.com>
2023-09-27 23:47:28 +0200

    another page

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -88,7 +88,7 @@ const Navbar = () => {
               <Link href='/contact'>CONTACT</Link>
             </li>
             <li onClick={handleNav} className='p-4 text-4xl hover:text-gray-500 font-verdana'>
-              <Link href='/'>CONTACT</Link>
+              <Link href='/threesixty'>360 View</Link>
             </li>
           </ul>
         </div>
