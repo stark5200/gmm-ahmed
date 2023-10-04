d43f77f16d4709c90176f8870f69907d5ce47ead
stark5200 <a.taha5200@gmail.com>
2023-10-04 23:42:03 +0200

    images source

--- src/components/SwiperComp.tsx
+++ src/components/SwiperComp.tsx
@@ -1,6 +1,7 @@
 // core version + navigation, pagination modules:
 import Swiper from 'swiper';
 import { Navigation, Pagination } from 'swiper/modules';
+import pictu
 // import Swiper and modules styles
 import 'swiper/css';
 import 'swiper/css/navigation';
@@ -11,4 +12,31 @@ const swiper = new Swiper('.swiper', {
   // configure Swiper to use modules
   modules: [Navigation, Pagination],
   ...
+
+  <div className="grid-cols-3 p-20 space-y-2 lg:space-y-0 lg:grid lg:gap-3 lg:grid-rows-3">
+        <div className="w-full rounded">
+          <Image src={"/pictures/2_Kapselhotel.jpg"}
+            alt="image" width={350} height={350}/>
+        </div>
+        <div className={"w-full col-span-2 row-span-2 rounded"}>
+          <Image src="/pictures/1_Kapselhotel.jpg"
+            alt="image" width={720} height={720} />
+        </div>
+        <div className="w-full rounded">
+          <Image src={"/pictures/3_Kapselhotel.jpg"}
+            alt="image" width={350} height={350}/>
+        </div>
+        <div className="w-full rounded">
+          <Image src={"/pictures/62_Kapselhotel.jpg"}
+            alt="image" width={350} height={350}/>
+        </div>
+        <div className="w-full rounded">
+          <Image src={"/pictures/61_Kapselhotel.jpg"}
+            alt="image" width={350} height={350}/>
+        </div>
+        <div className="w-full rounded">
+          <Image src={"/pictures/10_Kapselhotel.jpg"}
+            alt="image" width={350} height={350}/>
+        </div>
+      </div>
 });
