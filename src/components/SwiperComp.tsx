e8a3c28ac8858666a90e31577eebe1583d944212
stark5200 <a.taha5200@gmail.com>
2023-10-04 23:44:43 +0200

    added div

--- src/components/SwiperComp.tsx
+++ src/components/SwiperComp.tsx
@@ -18,6 +18,32 @@ const swiper = new Swiper('.swiper', {
 
 const SwiperComp = () {
   <h1>Swiper Comp</h1> 
+  <div className="grid-cols-3 p-20 space-y-2 lg:space-y-0 lg:grid lg:gap-3 lg:grid-rows-3">
+        <div className="w-full rounded">
+          <Image src={"/pictures/2_Kapselhotel.jpg"}
+            alt="image" width={350} height={350}/>
+        </div>
+        <div className={"w-full col-span-2 row-span-2 rounded"}>
+          <Image src="/pictures/1_Kapselhotel.jpg"
+            alt="image" width={720} height={720} />
+        </div>
+        <div className="w-full rounded">
+          <Image src={"/pictures/3_Kapselhotel.jpg"}
+            alt="image" width={350} height={350}/>
+        </div>
+        <div className="w-full rounded">
+          <Image src={"/pictures/62_Kapselhotel.jpg"}
+            alt="image" width={350} height={350}/>
+        </div>
+        <div className="w-full rounded">
+          <Image src={"/pictures/61_Kapselhotel.jpg"}
+            alt="image" width={350} height={350}/>
+        </div>
+        <div className="w-full rounded">
+          <Image src={"/pictures/10_Kapselhotel.jpg"}
+            alt="image" width={350} height={350}/>
+        </div>
+      </div>
 }
 
 export default SwiperComp
