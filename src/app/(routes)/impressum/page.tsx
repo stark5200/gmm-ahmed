fd19ea861c91624c18c00630ea3648062f5e646f
stark5200 <a.taha5200@gmail.com>
2023-10-06 22:36:28 +0200

    Impressum

---
+++ src/app/(routes)/impressum/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <>
+    <section className="pt-10 lg:pt-[120px] pb-12 lg:pb-[90px] overflow-hidden bg-emerald-50">
+      
+        <h1> Impressum </h1>
+      </section>
+    </>
+  )
+}
+
+export default page
