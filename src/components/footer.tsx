1603ec337600f3ae64258644645e87f6f97077a2
stark5200 <a.taha5200@gmail.com>
2023-10-06 22:44:00 +0200

    Agb

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import Image from "next/image";
+import { Link } from "react-router-dom";
 let thisYear = new Date().getFullYear()
 // hi
 function Footer() {
@@ -22,6 +23,8 @@ function Footer() {
         <div className="text-emerald-600 text-left align-left">
           <a href={`tel: +41 44 500 76 80`}>Tel +41 44 500 76 80</a>
           <p>info@greenmarmot.com</p>
+          <a href='/impressum'> Impressum </a>
+          <a href={"../public/AGB_GreenMarmotGmbH.pdf"}></a>
         </div>
       </div>
       <div>
