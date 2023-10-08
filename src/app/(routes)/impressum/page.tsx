d42d3030199191d01a55cc414daee240971c15c3
stark5200 <a.taha5200@gmail.com>
2023-10-08 23:36:08 +0200

    some feedback

--- src/app/(routes)/impressum/page.tsx
+++ src/app/(routes)/impressum/page.tsx
@@ -6,6 +6,8 @@ const page = () => {
     <section className="pt-10 lg:pt-[120px] pb-12 lg:pb-[90px] overflow-hidden bg-emerald-50">
       
         <h1> Impressum </h1>
+
+        <p>This page should include more info</p>
       </section>
     </>
   )
