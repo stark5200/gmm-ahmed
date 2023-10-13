6449510dbe3dd436fced75a9838b1908e3525f68
stark5200 <a.taha5200@gmail.com>
2023-10-13 20:45:27 +0200

    images in lib

---
+++ src/lib/images.tsx
@@ -0,0 +1,52 @@
+// Images
+import n1 from '@/public/pictures/1_Kapselhotel.jpg'
+import n2 from '@/public/pictures/2_Kapselhotel.jpg'
+import n3 from '@/public/pictures/3_Kapselhotel.jpg'
+import n4 from '@/public/pictures/61_Kapselhotel.jpg'
+import n5 from '@/public/pictures/5_Kapselhotel.jpg'
+import n6 from '@/public/pictures/6_Kapselhotel.jpg'
+import n7 from '@/public/pictures/7_Kapselhotel.jpg'
+import n8 from '@/public/pictures/8_Kapselhotel.jpg'
+import n9 from '@/public/pictures/9_Kapselhotel.jpg'
+import n10 from '@/public/pictures/10_Kapselhotel.jpg'
+import n11 from '@/public/pictures/62_Kapselhotel.jpg'
+import n12 from '@/public/pictures/12_Kapselhotel.jpg'
+import n13 from '@/public/pictures/13_Kapselhotel.jpg'
+import n14 from '@/public/pictures/14_Kapselhotel.jpg'
+import n15 from '@/public/pictures/2.jpg'
+import n16 from '@/public/pictures/5.jpg'
+import n17 from '@/public/pictures/6.jpg'
+import n18 from '@/public/pictures/7.jpg'
+import n19 from '@/public/pictures/9.jpg'
+import n20 from '@/public/pictures/10.jpg'
+import n21 from '@/public/pictures/11.jpg'
+import n22 from '@/public/pictures/12.jpg'
+import n23 from '@/public/pictures/13.jpg'
+import n24 from '@/public/pictures/14.jpg'
+
+export const images = [
+  { src: n1, alt: 'N1' },
+  { src: n2, alt: 'N2' },
+  { src: n3, alt: 'N3' },
+  { src: n4, alt: 'N4' },
+  { src: n5, alt: 'N5' },
+  { src: n6, alt: 'N6' },
+  { src: n7, alt: 'N7' },
+  { src: n8, alt: 'N8' },
+  { src: n9, alt: 'N9' },
+  { src: n10, alt: 'N10' },
+  { src: n11, alt: 'N11' },
+  { src: n12, alt: 'N12' },
+  { src: n13, alt: 'N13' },
+  { src: n14, alt: 'N14' },
+  { src: n15, alt: 'N15' },
+  { src: n16, alt: 'N16' },
+  { src: n17, alt: 'N17' },
+  { src: n18, alt: 'N18' },
+  { src: n19, alt: 'N19' },
+  { src: n20, alt: 'N20' },
+  { src: n21, alt: 'N21' },
+  { src: n22, alt: 'N22' },
+  { src: n23, alt: 'N23' },
+  { src: n24, alt: 'N24' }
+]
