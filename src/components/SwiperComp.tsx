eb29e1f8b412f80b26f811782c531daf99005321
stark5200 <a.taha5200@gmail.com>
2023-10-13 21:03:27 +0200

    first slider option

--- src/components/SwiperComp.tsx
+++ src/components/SwiperComp.tsx
@@ -1,27 +1,47 @@
+'use client'
+//Sliding pattern
+
+import Image from 'next/image'
+
 // core version + navigation, pagination modules:
-import Swiper from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper';
 import { Navigation, Pagination } from 'swiper/modules';
-import picture
+
+import { images } from "@/lib/images"
+
 // import Swiper and modules styles
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 
-// init Swiper:
-const swiper = new Swiper('.swiper', {
-  // configure Swiper to use modules
-  modules: [Navigation, Pagination],
-  ...
-
-  
-});
-
 const SwiperComp = () => {
 
-  <div >
-  <h1>Swiper Comp</h1> 
-       
+  return (
+    <section className='py-12'>
+      <div className='container'>
+        <Swiper
+          navigation
+          pagination={{ type: 'fraction' }}
+          modules={[Navigation, Pagination]}
+          onSwiper={swiper => console.log(swiper)}
+          className='h-96 w-full rounded-lg'
+        >
+          {images.map((image, index) => (
+            <SwiperSlide key={index}>
+              <div className='flex h-full w-full items-center justify-center'>
+                <Image
+                  src={image.src}
+                  alt={image.alt}
+                  className='block h-full w-full object-cover'
+                />
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+        <p> Slides </p>
       </div>
+    </section>
+  )
 }
 
 export default SwiperComp
